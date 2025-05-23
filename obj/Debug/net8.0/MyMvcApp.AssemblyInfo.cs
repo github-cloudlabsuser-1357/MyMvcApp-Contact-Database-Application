de191f90d554257615527479f2ce1574a8754fe9
github_cloudlabsuser_1357 <github_cloudlabsuser_1357@cloudlabsaiuser.com>
2025-05-23 13:08:26 +0000

    Update assembly information and binary files for MyMvcApp

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf310651cc103b4b2c7fc2ce7e602ecf169c4971")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3e1283da549ef29b7ffd7bffc6533f4b4c15b87")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
