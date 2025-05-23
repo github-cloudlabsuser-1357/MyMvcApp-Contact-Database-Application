3e5e6ed99233a73b5b185b5023c97487c5831b0c
github_cloudlabsuser_1357 <github_cloudlabsuser_1357@cloudlabsaiuser.com>
2025-05-23 10:40:42 +0000

    Refactor static web assets paths and remove unused jQuery validation assets
    - Updated paths in staticwebassets.pack.json to reflect new project structure.
    - Removed unused jQuery validation unobtrusive assets from msbuild.MyMvcApp.Microsoft.AspNetCore.StaticWebAssets.props.
    - Added initial settings.json for VS Code configuration.

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99867785418e4b7fee1e629a3b02a9bab10bfdfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f8466d4fe7c8cab21c454802e49eee092592a9b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
